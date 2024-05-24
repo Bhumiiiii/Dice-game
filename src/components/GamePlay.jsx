c8830e6828975995bc9cdc7adb909f47c4fcce02
Bhumi Gupta <bhumigupta175@gmail.com>
2024-05-24 21:43:15 +0530

    init

--- src/components/GamePlay.jsx
+++ src/components/GamePlay.jsx
@@ -2,15 +2,50 @@ import NumberSelector from "./NumberSelector";
 import RoleDice from "./RoleDice";
 import TotalScore from "./TotalScore";
 import styled from "styled-components"; 
+import { useState } from "react";
 
 const GamePlay = () => {
+  const [score, setScore] = useState(0);
+  const [selectedNumber,setSelectedNumber] = useState();
+  const [currentDice , setCurrentDice] = useState(1);
+  const [error, setError] = useState("");
+
+  const generateRandomNumber = (min, max) => {
+    console.log(Math.floor(Math.random() * (max - min) + min));
+return Math.floor(Math.random() * (max - min) + min);
+};
+
+const roleDice = () => {
+  if(!selectedNumber) {
+    setError("You have not selected any number");
+    return;
+  };
+  setError("");
+
+    const randomNumber = generateRandomNumber(1,7);
+    setCurrentDice((prev) => randomNumber);
+
+
+    if(selectedNumber == randomNumber){
+      setScore((prev) => prev + randomNumber);
+  }else {
+      setScore((prev) => prev - 2)
+  }
+  setSelectedNumber(undefined);
+    };
+
   return (
     <MainContainer>
         <div className="top-section">
-        <TotalScore />
-        <NumberSelector />
+        <TotalScore score={score} />
+        <NumberSelector  
+        error={error}
+        selectedNumber={selectedNumber}
+        setSelectedNumber={setSelectedNumber}
+        />
         </div>
-        <RoleDice />
+        <RoleDice currentDice={currentDice}
+        roleDice={roleDice} />
     </MainContainer>
   )
 }
--- src/components/RoleDice.jsx
+++ src/components/RoleDice.jsx
@@ -1,30 +1,24 @@
-import { useState } from "react";
+
 import styled from "styled-components";
 
-const RoleDice = () => {
-const [ currentDice , setCurrentDice ] = useState();
-
-    const generateRandomNumber = (min, max) => {
-        console.log(Math.floor(Math.random = (max - min) + min))
-    return Math.floor(Math.random() = (max - min) + min);
-    };
+const RoleDice = ({currentDice, roleDice}) => {
 
   return (
     <DiceContainer>
-        <div className="dice" onClick={() => generateRandomNumber(1,7)}>
-            <img src="/images/Dice/dice_1.png" alt="Dice 1" />
+        <div className="dice" onClick={roleDice}>
+            <img src={`/images/Dice/dice_${currentDice}.png`} alt="Dice " />
         </div>
         <p>Click on Dice to roll </p>
     </DiceContainer>
   )
 }
 
-export default RoleDice
+export default RoleDice;
 
 const DiceContainer = styled.div`
 margin-top: 48px;
 display: flex;
-justify-content: center;
+flex-direction: column;
 align-items: center;
 
 p{
